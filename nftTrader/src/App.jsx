4bf77b5d23f5fcb6ac26479e350cbaebd25c138f
YassineTazi2022 <114074945+YassineTazi2022@users.noreply.github.com>
2025-08-11 00:03:11 +0200

    feat: Add initial project structure with React, Vite, ESLint, and basic css.

---
+++ nftTrader/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>NFT Trading Game</h1>
+      {/* Game UI will be implemented here */}
+    </div>
+  );
+}
+
+export default App
